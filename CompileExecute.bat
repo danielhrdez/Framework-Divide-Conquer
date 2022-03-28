4d12f6e2ed32dc56fae16b1c2f5a99b35e1aa446
Daniel Hernandez de Leon <alu0101331720@ull.edu.es>
2022-03-28 22:32:37 +0100

    🤣

--- CompileExecute.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-@set files=
-for /r %%f in (*.cs) do @set files=!files! %%f
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe %files%
-MainProgram.exe %1 %2
-del MainProgram.exe
